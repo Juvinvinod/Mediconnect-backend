011f73573bdf7c74b1e395f8f2f37049b5cabda9
juvin vinod <124287134+Juvinvinod@users.noreply.github.com>
2023-11-24 09:59:33 +0530

    Added booking routes

--- src/repositories/bookingRepository.ts
+++ src/repositories/bookingRepository.ts
@@ -1,9 +1,40 @@
+import { NotFoundError } from '../common/errors/notFoundError';
 import { IBooking } from '../common/types/booking';
 import { Booking } from '../models/bookingModel';
 
 export class BookingRepository {
   //get slots belonging to the id
   async getDoctorSlots(id: string): Promise<IBooking[] | null> {
+    return await Booking.find({ doctor_id: id, status: 'upcoming' });
+  }
+
+  //book a slot
+  async bookSlot(
+    doctorId: string,
+    userId: string,
+    bookingId: string,
+    payId: string
+  ): Promise<IBooking> {
+    const document = await Booking.findOne({
+      _id: bookingId,
+    });
+
+    if (document) {
+      document.patient_id = userId;
+      document.payment = payId;
+      document.status = 'booked';
+      return await document.save();
+    }
+    throw new NotFoundError('Booking not found');
+  }
+
+  // get all docs based on userId
+  async getDocs(id: string) {
+    return await Booking.find({ patient_id: id });
+  }
+
+  // get all docs based on doctorId
+  async getDoctorDocs(id: string) {
     return await Booking.find({ doctor_id: id });
   }
 }
--- src/routes/doctorRoutes.ts
+++ src/routes/doctorRoutes.ts
@@ -3,8 +3,10 @@ import { DoctorController } from '../controllers/doctorController';
 import { validateRequest } from '../middlewares/validateRequest';
 import { doctorSignupValidator } from '../middlewares/doctorSignupValidator';
 import { verifyToken } from '../middlewares/tokenChecker';
+import { BookingController } from '../controllers/bookingController';
 
 const doctorController = new DoctorController();
+const bookingController = new BookingController();
 const router = Router();
 
 router.post(
@@ -21,5 +23,6 @@ router.patch('/unblockDoctor', verifyToken, doctorController.unblockDoctor); //u
 router.get('/profile', verifyToken, doctorController.getProfile); // get doctor profile
 router.put('/password', verifyToken, doctorController.updatePassword); // update user in the database
 router.post('/createSlot', verifyToken, doctorController.makeSlot); //create slots
+router.get('/getSlots', verifyToken, bookingController.getDoctorBookingDocs); // get all booking related to doctor
 
 export default router;
