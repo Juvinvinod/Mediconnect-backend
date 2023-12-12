15b283b6eb2b940b90c4e32b196d50b05bcbe1cf
juvin vinod <124287134+Juvinvinod@users.noreply.github.com>
2023-12-12 11:31:49 +0530

    CORS test

--- src/app.ts
+++ src/app.ts
@@ -44,6 +44,7 @@ const allowedOrigins = [
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
