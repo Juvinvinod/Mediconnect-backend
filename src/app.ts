3c5f0a2b123d28655e78d0593f4f931e7de64f59
juvin vinod <124287134+Juvinvinod@users.noreply.github.com>
2023-12-12 16:24:51 +0530

    CORS test

--- src/app.ts
+++ src/app.ts
@@ -44,28 +44,15 @@ const allowedOrigins = [
   'https://mediconnect-frontend.vercel.app',
   'https://mediconnect.juvin.in',
   'https://mediconnect.juvin.in/',
+  'mediconnect-frontend.vercel.app',
+  'http://mediconnect-frontend.vercel.app',
 ];
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
   // allowedHeaders: ['Authorization', 'Content-Type'],
-  allowedHeaders: [
-    'content-Type',
-    'Access-Control-Allow-Headers',
-    'X-Requested-With',
-    'authorization',
-    'Access-Control-Allow-Origin',
-  ],
+  allowedHeaders: '*',
 };
 
-app.use((req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'https://mediconnect-frontend.vercel.app'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 // middlewares
 app.use(cors(options));
 app.options('*', cors());
